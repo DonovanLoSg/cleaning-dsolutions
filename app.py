f2639c61242e365d33c77a6378a454637c49a057
Donovan Lo <donovanlo1174@gmail.com>
2020-07-28 10:04:37 +0000

    Re-map route name and path

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from dotenv import load_dotenv
 from bson import ObjectId
 import os
 import datetime
+import flask_login
+# from passlib.hash import pbkdf2_sha256
+
 
 # load in the variables in the .env file into our operating system environment
 load_dotenv()
@@ -16,12 +19,57 @@ client = pymongo.MongoClient(MONGO_URI)
 
 # define my db_name
 DB_NAME = os.environ.get('DB_NAME')
+USER_COLLECTION_NAME = os.environ.get('USER_COLLECTION_NAME')
 
 # read in the SESSION_KEY variable from the operating system environment
 SESSION_KEY = os.environ.get('SESSION_KEY')
 
 # set the session key
 app.secret_key = SESSION_KEY
+
+# --- User Authentication: Start  ----------------------------
+# create a login manager
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
+
+# User object
+# This user object basically represents one user
+
+
+class User(flask_login.UserMixin):
+    pass
+
+
+# user loader for the Flask-Login login manager
+@login_manager.user_loader
+def user_loader(email):
+    # find the user from the database by its email
+    user_data = client[DB_NAME][USER_COLLECTION_NAME].find_one({
+        'email': email
+    })
+    print(user_data)
+    if user_data:
+        # create a new user object
+        current_user = User()
+        current_user.id = user_data['email']
+        current_user.nickname = user_data['nickname']
+        current_user.admin = user_data['admin']
+        return current_user
+    else:
+        return None
+
+
+# encrypt user's password        <-- rmb to use your database to create a collection that stores registered users' info like passwords and emails
+def password_encryptor(user_password):
+    return pbkdf2_sha256.hash(user_password)
+
+# verify user's password        <-- then use this function to check user's password against the password in your collection
+
+
+def verify_password(user_input, encrypted_password):
+    return pbkdf2_sha256.verify(user_input, encrypted_password)
+
+# --- User Authentication: End ---------------------------------
 
 # START ROUTING
 
@@ -31,18 +79,22 @@ app.secret_key = SESSION_KEY
 #   allow user to click on one of these 5 default article titles
 #   to view the article.
 # Display a search panel to allow users to search
-#  using different methods: search by article titles, 
+#  using different methods: search by article titles,
 # search by cleaning location or search by tags.
-# When a search is executed, it will return the result 
+# When a search is executed, it will return the result
 # in a list containing article titles and cleaning locations.
 # Include an option to display all the articles.
-# Display a search panel to allow administrator to search 
-# using different methods: search by article titles, 
+# Display a search panel to allow administrator to search
+# using different methods: search by article titles,
 # search by cleaning location or search by tags.
 @app.route('/')
 def index():
-    return '<h1>Home</h1>'
+    return redirect(url_for("home"))
 
+
+@app.route('/home')
+def home():
+    return render_template("home.template.html")
 
 # Allow members and administrators to log in
 # through a login page accepting nickname and password.
@@ -52,16 +104,41 @@ def index():
 # else they will be redirected to a members’ access area.
 # If the validation is unsuccessful,
 # they will be alerted with a message and allowed to retry again.
-@app.route('/login')
-def login():
-    return '<h1>Login</h1>'
+#
+
+
+@app.route('/auth/login', methods=['GET', 'POST'])
+def login_user():
+    if request.method == 'POST':  # recieved as form submitted
+        return '<h1>Login Porcess</h1>'
+    else:
+        return '<h1>Login Form</h1>'
+    #     email = request.form['email']
+    #     password = request.form['password']
+    #     try:
+    #         # Check if the user is valid, this would go through a database.
+    #         if User.is_login_valid(email, password):
+    #             session['email'] = email
+    #             # When we redirect them, we already have data saved in the session
+    #             return '<h1>Login</h1>'
+    #     except Exception as e:
+    #         # Send user to an error page if something happened during login.
+    #         return '<h1>Login Error</h1>'
+
+    # return render_template("auth/login")  # This template shows a login form, only called if the request.method was not 'POST'.
+
+
+@login_manager.unauthorized_handler
+def unauthorized():
+    # do stuff
+    return '<h1>Unauthorised</h1>'
 
 
 # A instruction to log the user out and return to the home page
-@app.route('/logout')
+@app.route('/auth/logout')
 def logout():
-    return '<h1>Logout</h1>'
-
+    flask_login.logout_user()
+    return redirect(url_for('home'))
 
 # Include a link to the user registration page which accept
 # nickname, email address, password and password retype.
@@ -69,20 +146,23 @@ def logout():
 # the password must be at least eight characters and contain
 # at least one letter and one number, the password retype
 # must be same as the password.
-@app.route('/register')
+
+
+@app.route('/auth/register')
 def register():
     return '<h1>Register</h1>'
 
 
-# Allow member to view their own profile comprising of 
+# Allow member to view their own profile comprising of
 # nickname, email address and password.
-# Allow member to update their nickname 
+# Allow member to update their nickname
 # or reset their password.
-# Allow administrator to view their own profile 
+# Allow administrator to view their own profile
 # comprising of nickname, email address and password.
-# Allow administrator to update their nickname 
+# Allow administrator to update their nickname
 # or reset their password.
 @app.route('/my-profile')
+@flask_login.login_required
 def my_profile():
     return '<h1>My Profile</h1>'
 
@@ -100,36 +180,36 @@ def instructions():
 # Allow user to select one of the articles by clicking
 #  on the article titles from the search results
 #  to view the article.
-# Allow user to select one of the articles by clicking on 
+# Allow user to select one of the articles by clicking on
 # the article titles from the list to view the article.
-# Allow member to select one of the articles by clicking on 
+# Allow member to select one of the articles by clicking on
 # the article titles from the list to view the article.
-# Allow member to select one of the articles by clicking on 
+# Allow member to select one of the articles by clicking on
 # the corresponding delete button.
-# Allow member to delete the article by clicking on the 
+# Allow member to delete the article by clicking on the
 # delete confirmation button on the article itself.
-# When a search is executed, it will return the result 
+# When a search is executed, it will return the result
 # in a list containing article titles and cleaning locations.
-# Allow administrator to select one of article by clicking 
+# Allow administrator to select one of article by clicking
 # on the article titles from the search results to view the article.
 # Allow administrator to delete an article by clicking on the corresponding delete button.
 # Allow administrator to delete any article.
-@app.route('/list-articles')
+@app.route('/articles/list')
 def list_articles():
     return '<h1>Articles</h1>'
 
 
-# Display the article containing article titles, 
-# cleaning location, article content, cleaning items, 
+# Display the article containing article titles,
+# cleaning location, article content, cleaning items,
 # cleaning supplies and tags.
 # List the comments for individual articles
 # Display the count of validating vote.
-# Allow member to edit the article by clicking on the 
+# Allow member to edit the article by clicking on the
 # edit button on the article itself.
 # Allow member to add a comment on the article page.
-# Allow member to edit the comment he left 
+# Allow member to edit the comment he left
 # on the article page
-# Allow member to validate the article content by voting 
+# Allow member to validate the article content by voting
 # whether it works, somewhat works, or doesn’t work.
 # Allow member to change their validating votes.
 # Allow member to edit articles they contributed.
@@ -142,38 +222,43 @@ def list_articles():
 #  whether it works, somewhat works, or doesn’t work.
 # Allow administrator to change their validating votes.
 # Allow administrator to edit any article.
-@app.route('/show-article')
+@app.route('/articles/show/<id>')
 def show_article():
     return '<h1>Article #</h1>'
 
 
 # Display a list of all articles the member contributed.
-# Allow member to select one of the articles by clicking on 
+# Allow member to select one of the articles by clicking on
 # the article titles from the list to view the article.
-# Allow member to select one of the articles by clicking on 
+# Allow member to select one of the articles by clicking on
 # the corresponding delete button.
-# Allow member to delete the article by clicking on the 
+# Allow member to delete the article by clicking on the
 # delete confirmation button on the article itself.
-# Allow administrator to select one of article by clicking 
+# Allow administrator to select one of article by clicking
 # on the article titles from the search results to view the article.
 # Allow administrator to delete an article by clicking on the corresponding delete button.
 # Allow administrator to delete any article.
-@app.route('/my-articles')
+@app.route('/articles/list/my')
+@flask_login.login_required
 def my_articles():
     return '<h1>My Articles</h1>'
 
-# Include an article creation page accepting article titles, 
-# cleaning location, article content, cleaning items, 
+# Include an article creation page accepting article titles,
+# cleaning location, article content, cleaning items,
 # cleaning supplies and tags.
 # Include an article creation page accepting article titles,
 #  cleaning location, article content, cleaning items, cleaning supplies and tags.
-@app.route('/contribute-articles')
+
+
+@app.route('/articles/contribute')
+@flask_login.login_required
 def contribute_articles():
     return '<h1>Contribute Articles</h1>'
 
 
 # Allow administrator to manage the list of cleaning location.
-@app.route('/manage-cleaning-locations')
+@app.route('/cleaning-location/manage')
+@flask_login.login_required
 def manage_cleaning_locations():
     return '<h1>Cleaning Locations</h1>'
 
@@ -185,7 +270,8 @@ def manage_cleaning_locations():
 # Allow administrator to delete user profile.
 # Allow administrator to reset user password.
 # Allow administrator to assign admin rights.
-@app.route('/manage-users')
+@app.route('/users/manage')
+@flask_login.login_required
 def manage_users():
     return '<h1>Users</h1>'
 
