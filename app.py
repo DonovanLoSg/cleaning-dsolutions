2708b7536576af70656e167715f378016a0003d9
Donovan Lo <donovanlo1174@gmail.com>
2020-08-02 07:27:52 +0000

    Tidy up codes related to Users

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, request, session, redirect, url_for, flash
-import pymongo
-from dotenv import load_dotenv
-from bson import ObjectId
-import os
-import datetime
 import flask_login
+import datetime
+import os
+from bson import ObjectId
+from dotenv import load_dotenv
+import pymongo
+from flask import Flask, render_template, request,
+session, redirect, url_for, flash
 # from passlib.hash import pbkdf2_sha256
 
 
@@ -97,10 +98,15 @@ def index():
 def home():
     return render_template("home.template.html")
 
+# --------------------------------------------------
+# Login
+# --------------------------------------------------
 # Login Page
 # accept email address and password
-# successful verification will be shown a login successful message and redirect to home
-# unsuccessful verificaiton will be shown relevant error message and ask to retry
+# successful verification will be shown a login successful message
+# and redirect to home.
+# unsuccessful verificaiton will be shown relevant error message
+# and ask to retry
 
 
 @app.route('/auth/login', methods=['GET', 'POST'])
@@ -133,7 +139,8 @@ def login():
                 session['admin'] = current_user.admin
                 return redirect(url_for('home'))
     else:
-        # This template shows a login form, only called if the request.method was not 'POST'.
+        # This template shows a login form,
+        # only called if the request.method was not 'POST'.
         return render_template("/auth/login.template.html")
 
 
@@ -143,6 +150,9 @@ def unauthorized():
     return render_template("/unauthorized.template.html")
 
 
+# --------------------------------------------------
+# Logout
+# --------------------------------------------------
 # A instruction to log the user out and return to the home page
 @app.route('/auth/logout')
 def logout():
@@ -177,8 +187,10 @@ def register():
             if user_data:
                 # Email address found in the database
                 flash(
-                    "This email address has been used for registration. Please use other email address", category='danger')
-                return render_template("/auth/register.template.html", form=form)
+                    "This email address has been used for registration. " +
+                    "Please use other email address", category='danger')
+                return render_template("/auth/register.template.html",
+                                       form=form)
             else:
                 user_data = client[DB_NAME][USER_COLLECTION_NAME].insert_one({
                     'email': form.get('input-email'),
@@ -194,6 +206,9 @@ def register():
         return render_template("/auth/register.template.html")
 
 
+# --------------------------------------------------
+# My Profile
+# --------------------------------------------------
 # Allow the user to view their own profile
 # and update their nickanme and password
 @app.route('/users/my-profile', methods=['GET', 'POST'])
@@ -209,7 +224,8 @@ def my_profile():
             client[DB_NAME][USER_COLLECTION_NAME].update_one(
                 myquery, updatevalues)
             flash("Profile successfully updated", category='success')
-            return render_template("/users/my-profile.template.html", form=form)
+            return render_template("/users/my-profile.template.html",
+                                   form=form)
         else:
             # changes to passowrd, to check both passwods to be the same
             # if both password are not the same
@@ -217,7 +233,8 @@ def my_profile():
                 # if the two passwords are not the same
                 flash("The passwords do not match. Please retry.",
                       category='danger')
-                return render_template("/users/my-profile.template.html", form=form)
+                return render_template("/users/my-profile.template.html",
+                                       form=form)
             else:
                 myquery = {'email': form.get('input-email')}
                 updatevalues = {'$set': {'nickname': form.get(
@@ -225,7 +242,8 @@ def my_profile():
                 client[DB_NAME][USER_COLLECTION_NAME].update_one(
                     myquery, updatevalues)
                 flash("Profile successfully updated", category='success')
-                return render_template("/users/my-profile.template.html", form=form)
+                return render_template("/users/my-profile.template.html",
+                                       form=form)
     else:
         return render_template("/users/my-profile.template.html")
 
@@ -319,6 +337,9 @@ def contribute_articles():
     return render_template("/articles/contribute.template.html")
 
 
+# --------------------------------------------------
+# Cleaning Locations
+# --------------------------------------------------
 # Allow administrator to manage the list of cleaning location.
 @ app.route('/cleaning-locations/manage', methods=['GET', 'POST'])
 @ flask_login.login_required
@@ -329,8 +350,10 @@ def manage_cleaning_locations():
             {'_id': ObjectId(form.get('id'))})
         # render a form to allow editing of the selectedd cleaning location
         if form.get('action') == "edit":
-            return render_template("/cleaning-locations/edit.template.html", location_data=location_data)
-        # update the database with the changes to the selected cleaning location
+            return render_template("/cleaning-locations/edit.template.html",
+                                   location_data=location_data)
+        # update the database with the changes to
+        # the selected cleaning location
         elif form.get('action') == "edit-process":
             myquery = {'_id': ObjectId(form.get('id'))}
             updatevalues = {'$set': {'location': form.get('input-location')}}
@@ -340,7 +363,8 @@ def manage_cleaning_locations():
             return redirect("/cleaning-locations/manage")
         # display the record mark for deletion to ask for confirmation
         elif form.get('action') == "delete":
-            return render_template("/cleaning-locations/delete.template.html", location_data=location_data)
+            return render_template("/cleaning-locations/delete.template.html",
+                                   location_data=location_data)
         # remove the selected clearning location from the database
         elif form.get('action') == "delete-process":
             myquery = {'_id': ObjectId(form.get('id'))}
@@ -356,19 +380,19 @@ def manage_cleaning_locations():
         else:
             return redirect(url_for('error_encountered'))
     else:
-        # display the add cleaning location form and list out the cleaning locations in the databasse
+        # display the add cleaning location form and
+        # list out the cleaning locations in the databasse
         location_data = client[DB_NAME]['cleaning_locations'].find().sort(
             'location', pymongo.ASCENDING)
-        return render_template("/cleaning-locations/manage.template.html", location_data=location_data)
+        return render_template("/cleaning-locations/manage.template.html",
+                               location_data=location_data)
+
+# --------------------------------------------------
+# Users Administration
+# --------------------------------------------------
+# Allow administrators to manage the list of users.
 
 
-# Display a list of all the users.
-# Allow administrator to search for a user.
-# Display the selected user profile.
-# Allow administrator to edit the user profile (modify nickname).
-# Allow administrator to delete user profile.
-# Allow administrator to reset user password.
-# Allow administrator to assign admin rights.
 @ app.route('/users/manage', methods=['GET', 'POST'])
 @ flask_login.login_required
 def manage_users():
@@ -376,8 +400,13 @@ def manage_users():
         form = request.form
         user_data = client[DB_NAME][USER_COLLECTION_NAME].find_one(
             {'_id': ObjectId(form.get('id'))})
+        # Render a form to allow editing of the selected user
+        # This includes changing nickname, reset password and
+        # assigning administrator's rights
         if form.get('action') == 'edit':
-            return render_template("/users/edit.template.html", form=form, user_data=user_data)
+            return render_template("/users/edit.template.html",
+                                   form=form, user_data=user_data)
+        # update the database with the changes to the selected user
         elif form.get('action') == 'edit-process':
             if form.get('input-password') == "":
                 if form.get('input-admin') == 'isAdmin':
@@ -386,26 +415,39 @@ def manage_users():
                     adminrights = False
                 # no change in password, direct update nickname and/or admin
                 myquery = {'_id': ObjectId(form.get('id'))}
-                updatevalues = {'$set': {'nickname': form.get('input-nickname'), 'admin':adminrights}}
-                client[DB_NAME][USER_COLLECTION_NAME].update_one(myquery, updatevalues)
+                updatevalues = {'$set': {'nickname': form.get(
+                    'input-nickname'), 'admin': adminrights}}
+                client[DB_NAME][USER_COLLECTION_NAME].update_one(
+                    myquery, updatevalues)
                 flash("User details successfully updated", category='success')
-                return render_template("/users/edit.template.html", form=form, user_data=user_data)
+                return render_template("/users/edit.template.html",
+                                       form=form, user_data=user_data)
             else:
                 if not(form.get('input-password') == form.get('input-verify')):
-                    flash("The passwords do not match. Please retry.", category='danger')
-                    return render_template("/users/edit.template.html", form=form, user_data=user_data)
+                    flash("The passwords do not match. Please retry.",
+                          category='danger')
+                    return render_template("/users/edit.template.html",
+                                           form=form, user_data=user_data)
                 else:
                     myquery = {'_id': ObjectId(form.get('id'))}
                     if form.get('input-admin') == 'isAdmin':
                         adminrights = True
                     else:
                         adminrights = False
-                    updatevalues = {'$set': {'nickname': form.get('input-nickname'),'password': form.get('input-password'),'admin':adminrights}}
-                    client[DB_NAME][USER_COLLECTION_NAME].update_one(myquery, updatevalues)
+                    updatevalues = {'$set': {
+                        'nickname': form.get('input-nickname'),
+                        'password': form.get('input-password'),
+                        'admin': adminrights}}
+                    client[DB_NAME][USER_COLLECTION_NAME].update_one(
+                        myquery, updatevalues)
                     flash("Profile successfully updated", category='success')
-                    return render_template("/users/edit.template.html", form=form, user_data=user_data)
+                    return render_template("/users/edit.template.html",
+                                           form=form, user_data=user_data)
+        # display the record mark for deletion to ask for confirmation
         elif form.get('action') == 'delete':
-            return render_template("/users/delete.template.html", form=form, user_data=user_data)
+            return render_template("/users/delete.template.html",
+                                   form=form, user_data=user_data)
+        # remove the selected user from the database
         elif form.get('action') == "delete-process":
             myquery = {'_id': ObjectId(form.get('id'))}
             client[DB_NAME][USER_COLLECTION_NAME].delete_one(myquery)
@@ -414,11 +456,11 @@ def manage_users():
         else:
             return redirect(url_for('error_encountered'))
     else:
-
-        # display the add cleaning location form and list out the cleaning locations in the databasse
-        user_data = client[DB_NAME][USER_COLLECTION_NAME].find().sort('nickname', pymongo.ASCENDING)
-
-        return render_template("/users/manage.template.html",user_data=user_data)
+        # display the search box and list out the users in the databasse
+        user_data = client[DB_NAME][USER_COLLECTION_NAME].find().sort(
+            'nickname', pymongo.ASCENDING)
+        return render_template("/users/manage.template.html",
+                               user_data=user_data)
 
 
 # inbuilt function which handles exception like file not found
@@ -426,9 +468,11 @@ def manage_users():
 def not_found(e):
     return render_template('/file-not-found.template.html')
 
+
 @ app.route('/error-encountered')
 def error_encountered():
     return render_template('/error-encountered.template.html')
+
 
 # "magic code" -- boilerplate
 if __name__ == '__main__':
