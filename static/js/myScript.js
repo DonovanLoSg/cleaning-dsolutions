5d008cff698aeedcbb41648384db3ae693a46ff3
Donovan Lo <donovanlo1174@gmail.com>
2020-08-14 19:57:30 +0000

    Add autoclose for alert messages

--- static/js/myScript.js
+++ static/js/myScript.js
@@ -1,58 +1,58 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  // DataTables
+  $("#myTable").DataTable({
+    responsive: true,
+    paging: true,
+    searching: true,
+    ordering: true,
+    info: true,
+    stateSave: true,
+  });
 
-    // DataTables
-    $('#myTable').DataTable({
-        "responsive": true,
-        "paging":   true,
-        "searching": true,
-        "ordering": true,
-        "info":     true,
-        "stateSave": true
-    });
-
-    // Tooltip
-    $('[data-toggle="tooltip"]').tooltip()
-
-
-    
-} );
-
-
-// DataTables
-$("myTable").on("click", function(event){
-  event.preventDefault()
+  // Tooltip
+  $('[data-toggle="tooltip"]').tooltip();
 });
 
-if (document.getElementById('myTable2')) {
-    $('#myTable2').DataTable({
-        "responsive": true,
-        "paging":   false,
-        "searching": false,
-        "ordering": false,
-        "info":     false
-    } );
+// DataTables
+$("myTable").on("click", function (event) {
+  event.preventDefault();
+});
+
+if (document.getElementById("myTable2")) {
+  $("#myTable2").DataTable({
+    responsive: true,
+    paging: false,
+    searching: false,
+    ordering: false,
+    info: false,
+  });
 }
 
-$("#myTable").on("click", function(){alert('x')} )
+$("#myTable").on("click", function () {
+  alert("x");
+});
 
+// HTML editor for article contribution and edition
 
+tinymce.init({
+  selector: "textarea#input-content",
+  menubar: false,
+  plugins: [
+    "advlist autolink lists link image charmap print preview anchor",
+    "searchreplace visualblocks code fullscreen",
+    "insertdatetime media table paste code help wordcount",
+  ],
+  toolbar:
+    "undo redo | formatselect | " +
+    "bold italic backcolor | alignleft aligncenter " +
+    "alignright alignjustify | bullist numlist outdent indent | " +
+    "removeformat | help",
+});
 
+// auto close alert message
 
-
- tinymce.init({
-    selector: 'textarea#input-content',
-    menubar: false,
-    plugins: [
-        'advlist autolink lists link image charmap print preview anchor',
-        'searchreplace visualblocks code fullscreen',
-        'insertdatetime media table paste code help wordcount'
-    ],
-    toolbar: 'undo redo | formatselect | ' +
-    'bold italic backcolor | alignleft aligncenter ' +
-    'alignright alignjustify | bullist numlist outdent indent | ' +
-    'removeformat | help',
-    
+window.setTimeout(function() {
+    $(".alert").fadeTo(500, 0).slideUp(500, function(){
+        $(this).remove(); 
     });
-
-
-    
+}, 2000);
