c32ca59544fa32e539cf48b5628ba5c2ff6d951f
Donovan Lo <donovanlo1174@gmail.com>
2020-08-15 03:21:08 +0000

    Turn off Debug, generate requirements.txt, split css file

--- app.py
+++ app.py
@@ -816,6 +816,6 @@ def error_encountered():
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
-            debug=True)
+            debug=False)
 
 
