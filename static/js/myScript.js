f5a80b7825eb1a0ac34f1dae6e461a85121e1952
Donovan Lo <donovanlo1174@gmail.com>
2020-08-16 09:36:35 +0000

    Tidy up Javascript files

--- static/js/myScript.js
+++ static/js/myScript.js
@@ -1,58 +1,57 @@
 $(document).ready(function () {
-  // DataTables
-  $("#myTable").DataTable({
-    responsive: true,
-    paging: true,
-    searching: true,
-    ordering: true,
-    info: true,
-    stateSave: true,
-  });
+	// DataTables
+	$("#myTable").DataTable({
+		responsive: true,
+		paging: true,
+		searching: true,
+		ordering: true,
+		info: true,
+		stateSave: true,
+	});
 
-  // Tooltip
-  $('[data-toggle="tooltip"]').tooltip();
+	// Tooltip
+	$('[data-toggle="tooltip"]').tooltip();
 });
 
 // DataTables
 $("myTable").on("click", function (event) {
-  event.preventDefault();
+	event.preventDefault();
 });
 
 if (document.getElementById("myTable2")) {
-  $("#myTable2").DataTable({
-    responsive: true,
-    paging: false,
-    searching: false,
-    ordering: false,
-    info: false,
-  });
+	$("#myTable2").DataTable({
+		responsive: true,
+		paging: false,
+		searching: false,
+		ordering: false,
+		info: false,
+	});
 }
 
 $("#myTable").on("click", function () {
-  alert("x");
+	alert("x");
 });
 
 // HTML editor for article contribution and edition
 
 tinymce.init({
-  selector: "textarea#input-content",
-  menubar: false,
-  plugins: [
-    "advlist autolink lists link image charmap print preview anchor",
-    "searchreplace visualblocks code fullscreen",
-    "insertdatetime media table paste code help wordcount",
-  ],
-  toolbar:
-    "undo redo | formatselect | " +
-    "bold italic backcolor | alignleft aligncenter " +
-    "alignright alignjustify | bullist numlist outdent indent | " +
-    "removeformat | help",
+	selector: "textarea#input-content",
+	menubar: false,
+	plugins: [
+		"advlist autolink lists link image charmap print preview anchor",
+		"searchreplace visualblocks code fullscreen",
+		"insertdatetime media table paste code help wordcount",
+	],
+	toolbar: "undo redo | formatselect | " +
+		"bold italic backcolor | alignleft aligncenter " +
+		"alignright alignjustify | bullist numlist outdent indent | " +
+		"removeformat | help",
 });
 
 // auto close alert message
 
-window.setTimeout(function() {
-    $(".alert").fadeTo(500, 0).slideUp(500, function(){
-        $(this).remove(); 
-    });
+window.setTimeout(function () {
+	$(".alert").fadeTo(500, 0).slideUp(500, function () {
+		$(this).remove();
+	});
 }, 2000);
